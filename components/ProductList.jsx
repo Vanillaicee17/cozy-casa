2df995e85af92bf5b42501b285f13a1f61856dbe
Amey Santosh Gupte <114873466+Vanillaicee17@users.noreply.github.com>
2024-10-08 15:47:10 +0530

    Update ProductList.jsx
    padding changed

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -239,14 +239,14 @@ export default function ProductList() {
                             const total = p.basePrice;
                             return (
                                 <tr key={p._id} className="bg-white">
-                                    <td className="border p-2 text-center">{p.name}</td>
-                                    <td className="border p-2 text-center">{total}</td>
-                                    <td className="border p-2 text-center">
+                                    <td className="border px-2 py-1 text-center">{p.name}</td>
+                                    <td className="border px-2 py-1 text-center">{total}</td>
+                                    <td className="border px-2 py-1 text-center">
                                         <button onClick={() => tablebuttonAction("minus", p.name, p.quantity)} disabled={loadingAction} className="subtract cursor-pointer inline-block px-3 py-1 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">-</button>
                                         <span className="inline-block w-3 mx-3">{p.quantity}</span>
                                         <button onClick={() => tablebuttonAction("plus", p.name, p.quantity)} disabled={loadingAction} className="add cursor-pointer inline-block px-3 py-1 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">+</button>
                                     </td>
-                                    <td className="border p-2 text-center">{p.finalCost}</td>
+                                    <td className="border px-2 py-1 text-center">{p.finalCost}</td>
                                 </tr>
                             );
                         })}
